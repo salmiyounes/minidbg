fbd71d9a4d0a37c52b9b36bb81ea7fb9967ebd5b
salmi younes <shinigamir913@gmail.com>
2026-04-29 00:06:37 +0100

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,38 @@
 #include "bestline.h"
-#include <vector>
-#include <sstream>
 #include <iostream>
+#include <sstream>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <vector>
 
-std::vector<std::string> split (const std::string &s, char delim) {
-    std::vector<std::string> result;
-    std::stringstream ss (s);
-    std::string item;
+std::vector<std::string> split(const std::string &s, char delim) {
+  std::vector<std::string> result;
+  std::stringstream ss(s);
+  std::string item;
 
-    while (std::getline (ss, item, delim)) {
-        result.push_back (item);
-    }
+  while (std::getline(ss, item, delim)) {
+    result.push_back(item);
+  }
 
-    return result;
+  return result;
 }
 
-bool starts_with(const std::string & str, const std::string & sub, bool ignore_case=false)
-{
-    int str_len = str.size();
-    int sub_len = sub.size();
-    if (str_len < sub_len) return false;
+bool starts_with(const std::string &str, const std::string &sub,
+                 bool ignore_case = false) {
+  int str_len = str.size();
+  int sub_len = sub.size();
+  if (str_len < sub_len)
+    return false;
 
-    if (ignore_case)
-    {
-        return std::equal(sub.begin(), sub.end(),
-            str.begin(), [](char a, char b) {return std::tolower(a) == std::tolower(b); });
-    }
-    return str.compare(0, sub_len, sub) == 0;
+  if (ignore_case) {
+    return std::equal(sub.begin(), sub.end(), str.begin(), [](char a, char b) {
+      return std::tolower(a) == std::tolower(b);
+    });
+  }
+  return str.compare(0, sub_len, sub) == 0;
 }
 
 class debugger {
@@ -43,29 +44,27 @@ public:
   debugger(std::string prog_name, pid_t pid) : prog_name(prog_name), pid(pid) {}
 
   void run();
-  void handle_command(const std::string& line);
+  void handle_command(const std::string &line);
   void continue_execution();
 };
 
-void debugger::handle_command(const std::string& line) {
+void debugger::handle_command(const std::string &line) {
   auto args = split(line, ' ');
   auto command = args[0];
 
   if (starts_with(command, "continue")) {
     continue_execution();
-  }
-  else {
+  } else {
     std::cerr << "Unknown command\n";
   }
-  
 }
 
 void debugger::continue_execution() {
-    ptrace(PTRACE_CONT, pid, nullptr, nullptr);
+  ptrace(PTRACE_CONT, pid, nullptr, nullptr);
 
-    int wait_status;
-    auto options = 0;
-    waitpid(pid, &wait_status, options);
+  int wait_status;
+  auto options = 0;
+  waitpid(pid, &wait_status, options);
 }
 
 void debugger::run() {
@@ -81,7 +80,7 @@ void debugger::run() {
   }
 }
 
-void start_debugee(const std::string& prog_name) {
+void start_debugee(const std::string &prog_name) {
   if (ptrace(PTRACE_TRACEME, nullptr, nullptr, nullptr) < 0) {
     std::cerr << "Error in ptrace\n";
     return;
